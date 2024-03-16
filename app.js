bacc0c4568f1c3d5fca5a2cc97c889662eb51ffb
jinx-vi-0 <120160355+jinx-vi-0@users.noreply.github.com>
2024-03-17 04:15:50 +0530

    fix: Align image on about page

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+//mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 mongoose.set('strictQuery', false);
 
 
